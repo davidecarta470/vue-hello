384fafe4e14aab0d18f0b232a9117bd749156535
davidecarta470 <cartadavi@gmail.com>
2021-11-10 18:00:21 +0100

    correction data

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ const app = new Vue({
   el: '#app',
   data:{
     primoProgramma : 'Ciao mondo!!',
-    messaggio:'il primo programma con Vue è stato creato il ' + new Date().toLocaleString(),
+    messaggio:'il mio primo programma con Vue è stato creato il 10/11/21',
   
   },
 
