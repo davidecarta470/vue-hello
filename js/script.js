fc7d46b8035a5ca58cdcd832d470b6e31e828b85
davidecarta470 <cartadavi@gmail.com>
2021-11-10 17:56:44 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const app = new Vue({
+  el: '#app',
+  data:{
+    primoProgramma : 'Ciao mondo!!',
+    messaggio:'il primo programma con Vue è stato creato il ' + new Date().toLocaleString(),
+    coloreStile: 'rossobold'
+  },
+
+})
